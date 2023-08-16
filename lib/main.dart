3b1fad37fc02af012c847e843a02f92b360fbfd1
brianmujjuni <brianmujjuni@engineer.com>
2023-08-16 14:42:18 +0300

    wrapped the main app with providerscope

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:favorite_places/screens/places.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final colorScheme = ColorScheme.fromSeed(
   brightness: Brightness.dark,
@@ -26,7 +27,7 @@ final theme = ThemeData().copyWith(
 );
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
