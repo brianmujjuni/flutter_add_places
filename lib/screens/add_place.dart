4df6d156b1bc9324c6d0a40c125ba7b98c48bd0b
brianmujjuni <brianmujjuni@engineer.com>
2023-08-17 20:10:50 +0300

    configured info.plist to allow acces camera and files

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -49,6 +49,9 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
                 color: Theme.of(context).colorScheme.onBackground,
               ),
             ),
+            const SizedBox(
+              height: 16,
+            ),
             const ImageInput(),
             const SizedBox(
               height: 16,
