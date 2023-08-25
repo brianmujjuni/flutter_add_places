355d63485b482cc25ccaf0c2c98d84a01f22207b
brianmujjuni <brianmujjuni@engineer.com>
2023-08-25 16:04:15 +0300

    changed gogle api key

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -21,7 +21,7 @@ class _LocationInputState extends State<LocationInput> {
     }
     final lat = _pickedLocation!.latitude;
     final lng = _pickedLocation!.longitude;
-    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng=&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$lat,$lng&key=AIzaSyBknSZUpuvQS-D7gwfa-6OWDMhc4b4e2UM';
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng=&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$lat,$lng&key=AIzaSyDLcwxUggpPZo8lcbH0TB4Crq5SJjtj4ag';
   }
 
   void _getCurrentLocation() async {
@@ -58,7 +58,7 @@ class _LocationInputState extends State<LocationInput> {
       return;
     }
     final url = Uri.parse(
-        'https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=AIzaSyBknSZUpuvQS-D7gwfa-6OWDMhc4b4e2UM');
+        'https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=AIzaSyDLcwxUggpPZo8lcbH0TB4Crq5SJjtj4ag');
     final response = await http.get(url);
     final resData = json.decode(response.body);
     final address = resData['results'][0]['formatted_address'];
