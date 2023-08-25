64dac1e197edf98249d090966a2523b355b7fabc
brianmujjuni <brianmujjuni@engineer.com>
2023-08-25 15:38:16 +0300

    add placelocation model and snap shot

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -15,6 +15,15 @@ class _LocationInputState extends State<LocationInput> {
   PlaceLocation? _pickedLocation;
   var _isGettingLocation = false;
 
+  String get locationImage {
+    if (_pickedLocation == null) {
+      return '';
+    }
+    final lat = _pickedLocation!.latitude;
+    final lng = _pickedLocation!.longitude;
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng=&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$lat,$lng&key=AIzaSyBknSZUpuvQS-D7gwfa-6OWDMhc4b4e2UM';
+  }
+
   void _getCurrentLocation() async {
     Location location = Location();
 
@@ -33,7 +42,7 @@ class _LocationInputState extends State<LocationInput> {
     permissionGranted = await location.hasPermission();
     if (permissionGranted == PermissionStatus.denied) {
       permissionGranted = await location.requestPermission();
-    if (permissionGranted != PermissionStatus.granted) {
+      if (permissionGranted != PermissionStatus.granted) {
         return;
       }
     }
@@ -45,14 +54,18 @@ class _LocationInputState extends State<LocationInput> {
     locationData = await location.getLocation();
     final lat = locationData.latitude;
     final lng = locationData.longitude;
-
+    if (lat == null || lng == null) {
+      return;
+    }
     final url = Uri.parse(
-        'https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key= AIzaSyBknSZUpuvQS-D7gwfa-6OWDMhc4b4e2UM');
+        'https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=AIzaSyBknSZUpuvQS-D7gwfa-6OWDMhc4b4e2UM');
     final response = await http.get(url);
     final resData = json.decode(response.body);
     final address = resData['results'][0]['formatted_address'];
 
     setState(() {
+      _pickedLocation =
+          PlaceLocation(latitude: lat, longitude: lng, address: address);
       _isGettingLocation = false;
     });
   }
