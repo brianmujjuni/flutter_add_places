b6e0acd4669ebf7f0c363bcbfeb7f7baebd622a1
brianmujjuni <brianmujjuni@engineer.com>
2023-08-25 16:36:59 +0300

    added location to save function

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -6,6 +6,7 @@ import 'package:favorite_places/widgets/image_input.dart';
 import 'package:favorite_places/widgets/location_input.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:location/location.dart';
 
 class AddPlaceScreen extends ConsumerStatefulWidget {
   const AddPlaceScreen({super.key});
@@ -24,13 +25,16 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
 //save place
   void _savePlace() {
     final enteredTitle = _titleController.text;
-    if (enteredTitle.isEmpty || _selectedImage == null) {
+    if (enteredTitle.isEmpty ||
+        _selectedImage == null ||
+        _selectedLocation == null) {
       return;
     }
 
     ref.read(userPlacesProvider.notifier).addPlace(
           enteredTitle,
           _selectedImage!,
+          _selectedLocation!,
         );
     Navigator.of(context).pop();
   }
@@ -71,7 +75,11 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
             const SizedBox(
               height: 16,
             ),
-            const LocationInput(),
+            LocationInput(
+              onSelectLocation: (location) {
+                _selectedLocation = location;
+              },
+            ),
             const SizedBox(
               height: 16,
             ),
