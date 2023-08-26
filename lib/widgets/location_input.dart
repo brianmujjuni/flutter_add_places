47472da8713fad307e5239fa78092f26c0bc0bab
brianmujjuni <brianmujjuni@engineer.com>
2023-08-26 15:40:52 +0300

    added select and save place on map

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,19 +1,22 @@
-import 'package:favorite_places/models/place.dart';
-import 'package:favorite_places/screens/map.dart';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 import 'package:http/http.dart' as http;
-import 'dart:convert';
+
+import 'package:favorite_places/screens/map.dart';
+import 'package:favorite_places/models/place.dart';
 
 class LocationInput extends StatefulWidget {
   const LocationInput({super.key, required this.onSelectLocation});
 
-  //add selected location to other files
   final void Function(PlaceLocation location) onSelectLocation;
 
   @override
-  State<LocationInput> createState() => _LocationInputState();
+  State<LocationInput> createState() {
+    return _LocationInputState();
+  }
 }
 
 class _LocationInputState extends State<LocationInput> {
@@ -74,25 +77,29 @@ class _LocationInputState extends State<LocationInput> {
     setState(() {
       _isGettingLocation = true;
     });
+
     locationData = await location.getLocation();
     final lat = locationData.latitude;
     final lng = locationData.longitude;
+
     if (lat == null || lng == null) {
       return;
     }
+
     _savePlace(lat, lng);
   }
 
   void _selectOnMap() async {
     final pickedLocation = await Navigator.of(context).push<LatLng>(
       MaterialPageRoute(
-        builder: (ctx) =>const MapScreen(),
+        builder: (ctx) => const MapScreen(),
       ),
     );
 
     if (pickedLocation == null) {
       return;
     }
+
     _savePlace(pickedLocation.latitude, pickedLocation.longitude);
   }
 
@@ -118,31 +125,36 @@ class _LocationInputState extends State<LocationInput> {
     if (_isGettingLocation) {
       previewContent = const CircularProgressIndicator();
     }
+
     return Column(
       children: [
         Container(
-            height: 170,
-            width: double.infinity,
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              border: Border.all(
-                width: 1,
-                color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
-              ),
+          height: 170,
+          width: double.infinity,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 1,
+              color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
             ),
-            child: previewContent),
-        Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-          TextButton.icon(
-            onPressed: _getCurrentLocation,
-            icon: const Icon(Icons.location_on),
-            label: const Text('Get current user location'),
           ),
-          TextButton.icon(
-            onPressed: () {},
-            icon: const Icon(Icons.map_rounded),
-            label: const Text('Select on map'),
-          )
-        ]),
+          child: previewContent,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextButton.icon(
+              icon: const Icon(Icons.location_on),
+              label: const Text('Get Current Location'),
+              onPressed: _getCurrentLocation,
+            ),
+            TextButton.icon(
+              icon: const Icon(Icons.map),
+              label: const Text('Select on Map'),
+              onPressed: _selectOnMap,
+            ),
+          ],
+        ),
       ],
     );
   }
