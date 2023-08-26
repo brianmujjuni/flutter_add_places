43db0d8715573587a571f09a5678da7bc4ab8c57
brianmujjuni <brianmujjuni@engineer.com>
2023-08-26 11:40:39 +0300

    change minsdk version

--- lib/screens/place_detail.dart
+++ lib/screens/place_detail.dart
@@ -5,6 +5,11 @@ class PlaceDetailScreen extends StatelessWidget {
   const PlaceDetailScreen({super.key, required this.place});
 
   final Place place;
+  String get locationImage {
+    final lat = place.location.latitude;
+    final lng = place.location.longitude;
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng=&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$lat,$lng&key=AIzaSyDLcwxUggpPZo8lcbH0TB4Crq5SJjtj4ag';
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,11 +19,45 @@ class PlaceDetailScreen extends StatelessWidget {
         ),
         body: Stack(
           children: [
-            Image.file( 
+            Image.file(
               place.image,
               fit: BoxFit.cover,
               width: double.infinity,
-            )
+            ),
+            Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: Column(
+                  children: [
+                    CircleAvatar(
+                      radius: 70,
+                      backgroundImage: NetworkImage(locationImage),
+                    ),
+                    Container(
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 24,
+                        vertical: 16,
+                      ),
+                      decoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                            colors: [
+                              Colors.transparent,
+                              Colors.black54,
+                            ],
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter),
+                      ),
+                      child: Text(
+                        place.location.address,
+                        style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                              color: Theme.of(context).colorScheme.onBackground,
+                            ),
+                      ),
+                    )
+                  ],
+                ))
           ],
         ));
   }
